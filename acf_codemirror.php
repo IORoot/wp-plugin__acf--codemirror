af25226d071ae0307ad5afad56d3e87d891b39e8
Andy Pearson <andy@londonparkour.com>
2020-12-25 14:40:22 +0000

    Separate AndyP Main Menu

--- acf_codemirror.php
+++ acf_codemirror.php
@@ -15,10 +15,11 @@
 function codemirror_enqueue_scripts($hook)
 {
 
+    /** Add the codemirror script & style */
     wp_enqueue_script('wp-theme-plugin-editor');
     wp_enqueue_style('wp-codemirror');
 
-
+    /** Configure the codemirror default settings */
     $codemirror_settings = array(
         'type' => 'text/x-php',
         'codemirror' => [
@@ -30,11 +31,15 @@ function codemirror_enqueue_scripts($hook)
             'foldGutter' => true,
             'height' => 40,
             'indentUnit' => 4,
-            'lineWrapping' => false,
+            'lineWrapping' => true,
+            'refresh' => true,
         ],   
     );
 
+    /** Add the bits needed for the code editor to work. */
     $cm_settings['codeEditor'] = wp_enqueue_code_editor($codemirror_settings);
+
+    /** Localize the $cm_settings object to be used on-page. */
     wp_localize_script('wp-theme-plugin-editor', 'cm_settings', $cm_settings);
 
 }
@@ -42,28 +47,35 @@ function codemirror_enqueue_scripts($hook)
 add_action('admin_enqueue_scripts', 'codemirror_enqueue_scripts');
 
 
-
+/**
+ * Add the custom Codemirror Javascript to the page footer.
+ */
 function codemirror_js()
 {
     ?>
     <script type="text/javascript">
         jQuery(document).ready(function ($) {
 
+            /** For every textarea with the class, initialise codemirror */
             $('.ue__codemirror textarea').each( function( index ) {
-
                 wp.codeEditor.initialize(
                     this, cm_settings
                 );
             });
-        })
+            
+        });
+
+        
+                
     </script>
     <?php
 }
-    
-
 add_action('acf/input/admin_footer', 'codemirror_js');
 
 
+/**
+ * Add the custom Codemirror CSS into the header.
+ */
 function codemirror_css()
 {
     ?>
