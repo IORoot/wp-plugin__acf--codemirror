8dba1fac0aac652653f77f12370baeb6dab053b0
Andy Pearson <andy@londonparkour.com>
2021-09-21 12:31:26 +0100

    update for cleanup and readme

--- src/acf/acf_admin_head.php
+++ src/acf/acf_admin_head.php
@@ -15,6 +15,7 @@ function codemirror_css()
     <style type="text/css">
         <?php 
             include ANDYP_ACF_CODEMIRROR.'css/material.css'; 
+            include ANDYP_ACF_CODEMIRROR.'css/table_shadows.css'; 
         ?>
     </style>
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.8.55/css/materialdesignicons.min.css" rel="stylesheet">
